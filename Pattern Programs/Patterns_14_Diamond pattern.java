0e050a556f5f3eb2672d331346d802f2519a03e1
Harini A <hari628122004@gmail.com>
2025-06-23 15:15:33 +0530

    Create Patterns_14_Diamond pattern.java
    OUTPUT:
    
    Enter a number: 
    4
    
       * 
      * * 
     * * * 
    * * * * 
    * * * * 
     * * * 
      * * 
       *

---
+++ Pattern Programs/Patterns_14_Diamond pattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        
+        for(int a=1;a<=n;a++){
+            for(int b=1;b<=(n-a);b++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=a;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n-(i-1);k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+           
+        }
+    }
+} 
